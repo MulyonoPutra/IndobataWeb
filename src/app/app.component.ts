31cf3b06147623f1a10c1e8d0b80bc8bc5de0566
Mulyono Putra <mulyonoputra1994@gmail.com>
2020-05-02 15:21:15 +0700

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'indobata';
+
+
+
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    const menuToggle = document.querySelector('.menu-toggle input');
+    const nav = document.querySelector('nav ul');
+    const jumbotron = document.querySelector('jumbotron');
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('slide');
+        jumbotron.classList.remove();
+
+    });
+  }
+  
 }
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { OurClientsComponent } from './component/our-clients/our-clients.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { HomeComponent } from './component/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    AboutUsComponent,
+    OurClientsComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'product', component: ProductComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'about-us', component: AboutUsComponent},
+      {path: 'contact-us', component: ContactUsComponent},
+      {path: 'our-clients', component: OurClientsComponent}
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
