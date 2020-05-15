d3d5ee850e638011807cf3c202bacc0dfc091407
MulyonoPutra <mulyonoputra1994@gmail.com>
2020-05-16 03:28:35 +0700

    Show Image Product

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
 
-  constructor() { }
+export class ProductComponent implements OnInit {
+  public emptyImage = true;
+  public gentengBeton = false;
+  public pavingBlock = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.emptyImage = true;
   }
 
+  showGentengImage() {
+    this.gentengBeton = true;
+    this.pavingBlock = false;
+    this.emptyImage = false;
+  }
+
+  showPavingImage() {
+    this.pavingBlock = true;
+    this.gentengBeton = false;
+    this.emptyImage = false;
+  }
 }
