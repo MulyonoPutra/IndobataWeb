c08d2e616a4ecb8b18fd7a8ada10a197ee04441e
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-06-01 18:29:56 +0700

    Using Single Files Components.

--- src/app/component/about-us/about-us.component.ts
+++ src/app/component/about-us/about-us.component.ts
@@ -2,13 +2,199 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
-  templateUrl: './about-us.component.html'
+  template: `
+    <div class="jumbotron" style="color: #40a11ade;">
+      <div
+        class="title-header animate__animated animate__zoomInDown font-poppins"
+      >
+        <h1>About Us</h1>
+        <div style="color: #ffffff23;">
+          <h2>Indobata</h2>
+          <h3>Concrete Roof Tile & Concrete Block Manufacturing</h3>
+        </div>
+      </div>
+    </div>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="card-deck" data-aos="zoom-out-left">
+            <div class="card col border-col">
+              <div class="card-body">
+                <h2 class="font-poppins">
+                  We are the expart on this field better solution since 1984.
+                </h2>
+                <p class="paragraf1">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum.
+                </p>
+              </div>
+            </div>
+            <div class="card col border-col">
+              <div class="card-body">
+                <div>
+                  <img src="assets/img/network.svg" class="au-img" />
+                  <div>
+                    <h3 class="left font-poppins" style="font-size: 25px;">
+                      Vision
+                    </h3>
+                    <p class="paragraf">
+                      Indobata selalu menghadirkan produk beton berkualitas
+                      prima, dekoratif, serta ketahanan produk dalam segala
+                      cuaca, dan mampu memenuhi kebutuhan pasar yang ada.
+                    </p>
+                  </div>
+                </div>
+
+                <div>
+                  <img src="assets/img/business.svg" class="au-img" />
+                  <div>
+                    <h3 class="left font-poppins" style="font-size: 25px;">
+                      Mission
+                    </h3>
+                    <p class="paragraf">
+                      Tak hanya mengembangkan trend desain arsitektur dunia.
+                      Namun, Indobata juga mampu memproduksi produk yang
+                      inovatif sesuai permintaan customer dan kebutuhan pasar.
+                      Indobata akan terus meningkatkan alat produksi dengan
+                      standar terbaik, mengembangkan pelayanan agar mampu
+                      mambangun dan memelihara kemitraan dengan customer. Serta
+                      dapat mencapai kepuasan dan mempermudah customer untuk
+                      mendapatkan informasi tentang kualitas produk,
+                      ketersediaan produk, maupun waktu pengiriman produk.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-light bg-section">
+      <div class="container" data-aos="zoom-out-right">
+        <h1 class="font-poppins" style="text-align: center;">Why Choose Us</h1>
+
+        <div class="card-group">
+          <div
+            class="card border-col rounded animate__animated animate__bounceInLeft"
+          >
+            <img
+              class="card-img-top why-choose-us mt-3"
+              src="assets/img/dollar.svg"
+            />
+            <div class="card-body">
+              <h5 class="card-title" style="text-align: center;">
+                Professional Experience
+              </h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+          </div>
+          <div
+            class="card ml-2 border-col rounded animate__animated animate__bounceInDown"
+          >
+            <img
+              class="card-img-top why-choose-us mt-3"
+              src="assets/img/man.svg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title" style="text-align: center;">
+                Clients Satisfaction
+              </h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+          </div>
+          <div
+            class="card ml-2 border-col rounded rounded animate__animated animate__bounceInRight"
+          >
+            <img
+              class="card-img-top why-choose-us mt-3"
+              src="assets/img/user.svg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title" style="text-align: center;">
+                Dedicated To Clients
+              </h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card-group mt-2">
+          <div
+            class="card border-col rounded animate__animated animate__bounceInLeft"
+          >
+            <img
+              class="card-img-top why-choose-us mt-3"
+              src="assets/img/medal.svg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title" style="text-align: center;">
+                Fince Winner
+              </h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+          </div>
+          <div
+            class="card ml-2 border-col rounded animate__animated animate__bounceInDown"
+          >
+            <img
+              class="card-img-top why-choose-us mt-3"
+              src="assets/img/services1.svg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title" style="text-align: center;">
+                Outstanding Services
+              </h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+          </div>
+          <div
+            class="card ml-2 border-col rounded animate__animated animate__bounceInRight"
+          >
+            <img
+              class="card-img-top why-choose-us mt-3"
+              src="assets/img/cup.svg"
+              alt="Card image cap"
+            />
+            <div class="card-body" style="text-align: center;">
+              <h5 class="card-title">Best Support Winner</h5>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+            </div>
+          </div>
+        </div>
+        &nbsp;
+      </div>
+    </section>
+  `,
 })
 export class AboutUsComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -2,7 +2,232 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  template: `<div>
+    <div class="jumbotron" style="color: #40a11ade;">
+      <div
+        class="title-header animate__animated animate__zoomInDown font-poppins"
+      >
+        <h1>Indobata</h1>
+        <h2 style="color: #6b6363a1;">
+          Concrete Roof Tile & Concrete Block Manufacturing
+        </h2>
+      </div>
+    </div>
+
+    <section
+      class="mb-4 animate__animated animate__flipInY"
+      data-aos="flip-left"
+    >
+      <br />
+      <div id="article" class="row justify-content-center rounded-top">
+        <div class="card vision mr-2 animate__fadeInTopLeft">
+          <img
+            class="card-img-top mb-2 mt-3"
+            srcset="assets/img/business.png"
+            alt="Save Money"
+          />
+          <h4
+            class="font-poppins"
+            style="text-align: center; color: rgb(36, 35, 35);"
+          >
+            Save Money
+          </h4>
+          <div class="card-body">
+            <p class="card-text" style="text-align: center;">
+              Produk dengan bahan beton berkualitas dan mampu bersaing dengan
+              perusahaan konstruksi nasional, Indobata mampu memberikan harga
+              jual yang terjangkau oleh masyarakat.
+            </p>
+          </div>
+        </div>
+        <div class="card vision mr-2">
+          <img
+            class="card-img-top mb-2 mt-3"
+            src="assets/img/medal.png"
+            alt="Good Quality"
+          />
+          <h4
+            class="font-poppins"
+            style="text-align: center; color: rgb(36, 35, 35);"
+          >
+            Good Quality
+          </h4>
+          <div class="card-body">
+            <p class="card-text" style="text-align: center;">
+              Menggunakan bahan baku bermutu dengan pengawasan yang baik sesuai
+              standar yang ada sehingga banyak digunakan dalam berbagai proyek,
+              baik berskala nasional maupun lokal.
+            </p>
+          </div>
+        </div>
+        <div class="card vision me-2">
+          <img
+            class="card-img-top mb-2 mt-3"
+            src="assets/img/management.png"
+            alt="Innovation"
+          />
+          <h4
+            class="font-poppins"
+            style="text-align: center; color: rgb(36, 35, 35);"
+          >
+            Innovation
+          </h4>
+          <div class="card-body">
+            <p class="card-text" style="text-align: center;">
+              Indobata terus melakukan inovasi dalam mengembangkan produk hingga
+              tidak ketinggalan trend desain arsitektur yang terus berkembang.
+            </p>
+          </div>
+        </div>
+      </div>
+      <br />
+    </section>
+    <!-- <hr class="line"> -->
+
+    <div class="mb-4" style="color: rgb(109, 109, 107);">
+      <div class="container">
+        <article style="text-align: center;">
+          <p>
+            CV. Indobata berdiri sejak tahun 2003
+            <br />
+            merupakan perusahaan yang memproduksi bahan konstruksi berupa
+            genteng beton, ubin, paving block (conblock),
+            <br />
+            batako press, buis beton (gorong-gorong), roster atau loster,
+            kansteen (kanstin), serta genteng keramik berbagai merk.
+            <br />
+            Dengan harga yang relatif murah namun kami tidak melupakan nilai
+            estetika dari suatu produk,
+            <br />
+            kami memilih bahan baku dengan kualitas terbaik, Dengan harga
+            terjangkau, relatif murah.
+            <br />
+            Akan tetapi, Indobata tetap mengedepankan nilai estetika serta
+            menggunakan bahan baku dengan kualitas terbaik yang telah
+            terstandarisasi.
+          </p>
+        </article>
+      </div>
+    </div>
+    <!-- <hr class="line"> -->
+
+    &nbsp; &nbsp;
+    <section id="our-product" class="mb-4">
+      <h1 style="text-align: center; font-size: 35px;" class="font-poppins">
+        Produk Kami
+      </h1>
+      &nbsp;
+      <div
+        data-aos="zoom-in-down"
+        class="row justify-content-center rounded-top"
+      >
+        <div class="card ex-product mr-2 zoom" style="width: 20rem;">
+          <img
+            class="card-img-top"
+            src="assets/img/ubin.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <p class="card-text" style="text-align: center;">Ubin Difable</p>
+          </div>
+        </div>
+        <div
+          data-aos="zoom-in"
+          class="card ex-product mr-2 zoom"
+          style="width: 20rem;"
+        >
+          <img
+            class="card-img-top"
+            src="assets/img/kanstin.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <p class="card-text mt-4" style="text-align: center;">Kanstin S</p>
+          </div>
+        </div>
+        <div
+          data-aos="zoom-in"
+          class="card ex-product mr-2 zoom"
+          style="width: 20rem;"
+        >
+          <img
+            class="card-img-top"
+            src="assets/img/batako.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <p class="card-text mt-4" style="text-align: center;">
+              Batako Press
+            </p>
+          </div>
+        </div>
+      </div>
+      &nbsp; &nbsp;
+      <div style="text-align: center;" class="col-md-auto">
+        <button
+          id="btn-product"
+          class="btn btn-primary border"
+          [routerLink]="['/product']"
+          routerLinkActive="router-link-active"
+        >
+          View All Product
+        </button>
+      </div>
+
+      &nbsp;
+    </section>
+
+    &nbsp; &nbsp;
+    <section id="marketplace" style="min-height: 320px;" class="bg-section">
+      <div class="container">
+        <div class="row pb-4 mb-5">
+          <div class="col mb-5 text-center" style="margin-left: 40px;">
+            &nbsp;
+            <h1 class="font-poppins" style="text-align: center;">
+              Tersedia di Marketplace
+            </h1>
+            <div class="row mt-5" data-aos="zoom-in-down">
+              <a href="https://www.tokopedia.com/" target="_blank">
+                <img
+                  class="col card-img-top brand1"
+                  src="assets/img/tokopedia.png"
+                  alt="tokopedia"
+                />
+              </a>
+              <a href="https://www.bukalapak.com/" target="_blank">
+                <img
+                  class="col card-img-top brand"
+                  src="assets/img/bukalapak.png"
+                  alt="Bukalapak"
+                />
+              </a>
+              <a href="https://www.lazada.com/" target="_blank">
+                <img
+                  class="col card-img-top brand"
+                  src="assets/img/lazada.png"
+                  alt="Lazada"
+                />
+              </a>
+              <a href="https://www.shopee.com/" target="_blank">
+                <img
+                  class="col card-img-top brand"
+                  src="assets/img/shopee.png"
+                  alt="Shopee"
+                />
+              </a>
+              <a href="https://www.blibli.com/" target="_blank">
+                <img
+                  class="col card-img-top brand"
+                  src="assets/img/blibli.png"
+                  alt="Blibli"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div> `,
 })
 export class HomeComponent implements OnInit {
   constructor() {}
