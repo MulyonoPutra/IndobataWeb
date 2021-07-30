4b9bbc987704706fed1bb2f1de0d509fe57be750
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-07-30 22:23:05 +0700

    GET Clients

--- src/app/pages/our-clients/our-clients.component.ts
+++ src/app/pages/our-clients/our-clients.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Clients, IClients } from 'src/app/core/domain/entities/clients';
+import { ClientsRepository } from 'src/app/core/repositories/clients-repository';
+import { DataUtils } from 'src/app/core/services/utils/data-utils.service';
 
 @Component({
   template: `<div class="jumbotron" style="color: #40a11ade;">
@@ -13,147 +17,128 @@ import { Component, OnInit } from '@angular/core';
       </div>
     </div>
 
-    <div class="container mb-5">
-      <div class="card-group">
-        <div data-aos="zoom-in-down" class="card ml-2 shadow zoom">
-          <img
-            src="assets/img/tiara.jpg"
-            class="card-img-top oc-img"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">
-              Perumahan Tiara Regency
-            </h5>
-            <p class="card-text">
-              Limbangan, Kec. Sukaraja, Sukabumi Regency, Jawa Barat 43192
-            </p>
-            <p class="card-text">
-              <a
-                href="https://www.facebook.com/tiararegencysukabumi/"
-                target="blank"
-              >
-                <small class="text-muted"> Details </small>
-              </a>
-            </p>
-          </div>
-        </div>
-        <div data-aos="zoom-in-down" class="card ml-2 shadow zoom">
-          <img
-            src="assets/img/jugala.jpg"
-            class="card-img-top oc-img"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">
-              Jugala Residence
-            </h5>
-            <p class="card-text">
-              Batununggal, Kec. Cibadak, Sukabumi Regency, Jawa Barat 43351
-            </p>
-            <p class="card-text">
-              <a href="" target="blank">
-                <small class="text-muted"> Details </small>
-              </a>
-            </p>
-          </div>
-        </div>
-        <div data-aos="zoom-in-down" class="card ml-2 shadow zoom">
-          <img src="assets/img/bnn.png" class="card-img-top oc-img" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">BNN Lido</h5>
-            <p class="card-text">
-              Jl. HR Edi Sukma, Watesjaya, Cigombong, Bogor, Jawa Barat 16110
-            </p>
-            <p class="card-text">
-              <a
-                href="https://rehabilitasi.bnn.go.id/public/about"
-                target="blank"
-              >
-                <small class="text-muted"> Details </small>
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
+    <div class="container">
+      <div class="row">
+        <div
+          class="mt-3 col-md-4 position-relative"
+          *ngFor="let client of clients"
+        >
+          <div class="card box" style="width: 21rem" data-aos="zoom-in">
+            <img
+              [src]="
+                'data:' + client.imagesContentType + ';base64,' + client.images
+              "
 
-      <div class="card-group mt-3">
-        <div data-aos="zoom-in-up" class="card ml-2 shadow zoom">
-          <img
-            src="assets/img/benda.jpg"
-            class="card-img-top oc-img"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">
-              PT. Anugerah Bangun Sentosa
-            </h5>
-            <p class="card-text">
-              Perum Griya Benda Asri 2 blok CE 1, Benda, Kec. Cicurug, Sukabumi
-              Regency, Jawa Barat 43359
-            </p>
-            <p class="card-text">
-              <a
-                href="https://www.google.com/maps/uv?hl=id&pb=!1s0x2e69cc9152bec8ad%3A0xc25edabcc058dcd8!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPigY6aX3o-RZkxqi5-qOE90CLz-Wf25kQWt246%3Dw237-h200-k-no!5sperum%20griya%20benda%20asri%20-%20Penelusuran%20Google!15sCgIgAQ&imagekey=!1e10!2sAF1QipPigY6aX3o-RZkxqi5-qOE90CLz-Wf25kQWt246&sa=X&ved=2ahUKEwif_JuT8bzpAhUHXSsKHY1oDuMQoiowCnoECBAQBg  "
-                target="blank"
-              >
-                <small class="text-muted" style="margin-bottom: auto;">
-                  Details
-                </small>
-              </a>
-            </p>
+              class="card-img-top"
+            />
+            <div class="card-body">
+              <h5 class="card-title">{{ client?.name }}</h5>
+              <p class="text" [innerHTML]="client.address"></p>
+            </div>
+            <div class="card-footer">
+              <div class="d-flex bd-highlight mb-3">
+                <div class="font-size me-auto p-2 bd-highlight">
+                  <!-- {{ post?.createdDate | date: "short" }} -->
+                  <!--            {{ calculateDiff(post) }}
+                  {{ hourTime(post) }} -->
+                </div>
+                <div class="font-size p-2 bd-highlight"></div>
+                <div class="font-size p-2 bd-highlight">
+                  <!--   {{ post?.author }} -->
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div data-aos="zoom-in-up" class="card ml-2 shadow zoom">
-          <img
-            src="assets/img/adhita.jpg"
-            class="card-img-top oc-img"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">
-              PT. Adhita Karya Pratama
-            </h5>
-            <p class="card-text">
-              Jl Kartika Puspa Raya No.23, Bojong Baru, Kec. Bojong Gede, Bogor,
-              Jawa Barat 16920
-            </p>
-            <p class="card-text">
-              <a
-                href="https://www.google.com/search?safe=strict&rlz=1C1CHBD_enID900ID900&sxsrf=ALeKk00s1E1K3f5ugOehgl8Xn5VGvZyQcA:1589786830014&q=graha%20kartika%20puspa%20raya&sa=X&ved=2ahUKEwiT2ZW68bzpAhX6zjgGHTxSDIEQvS4wAHoECAwQIA&biw=1536&bih=722&npsic=0&rflfq=1&rlha=0&rllag=-6489523,106803374,1241&tbm=lcl&rldimm=16262552073857839518&lqi=ChhncmFoYSBrYXJ0aWthIHB1c3BhIHJheWFaNAoYZ3JhaGEga2FydGlrYSBwdXNwYSByYXlhIhhncmFoYSBrYXJ0aWthIHB1c3BhIHJheWE&rldoc=1&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2&rlst=f#rlfi=hd:;si:16262552073857839518,l,ChhncmFoYSBrYXJ0aWthIHB1c3BhIHJheWFaNAoYZ3JhaGEga2FydGlrYSBwdXNwYSByYXlhIhhncmFoYSBrYXJ0aWthIHB1c3BhIHJheWE;mv:[[-6.4808892,106.8131164],[-6.5099795,106.7936335]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2"
-                target="blank"
-              >
-                <small class="text-muted"> Details </small>
-              </a>
-            </p>
-          </div>
-        </div>
-        <div data-aos="zoom-in-up" class="card ml-2 shadow zoom">
-          <img
-            src="assets/img/ciangsana.jpg"
-            class="card-img-top oc-img"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title" style="text-align: center;">
-              Kawasan Industri Cikembar
-            </h5>
-            <p class="card-text">
-              Jl. Perintis Kemerdekaan Km. 6 KP Cimenteng RT 001 RW 005,
-              Cikembar, Sukabumi, West Java 43157
-            </p>
-            <p class="card-text">
-              <a href="" target="blank">
-                <small class="text-muted"> Details </small>
-              </a>
-            </p>
-          </div>
-        </div>
+        <div class="col-md-3"></div>
       </div>
     </div> `,
+  styles: [
+    `
+      .logo-size {
+        height: 300px;
+        width: 300px;
+      }
+
+      .text {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        /* number of lines to show */
+        -webkit-box-orient: vertical;
+      }
+
+      .font-size {
+        font-size: 10px !important;
+      }
+
+      .card-footer {
+        padding: 0.5rem 1rem;
+        background-color: #ffffff;
+        border-top: 1px solid rgba(0, 0, 0, 0.125);
+      }
+
+      .card {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        min-width: 0;
+        word-wrap: break-word;
+        background-color: #fff;
+        background-clip: border-box;
+        border: 1px solid #dc354500;
+        height: 70vh !important;
+      }
+
+      .card-img-top {
+        border-radius: 10px;
+      }
+
+      img {
+        float: left;
+        width: 350px;
+        height: 300px;
+        object-fit: cover;
+      }
+    `,
+  ],
 })
 export class OurClientsComponent implements OnInit {
-  constructor() {}
+  clients: Clients[] = [];
 
-  ngOnInit(): void {}
+  public client: Clients;
+  constructor(
+    private clientService: ClientsRepository,
+    protected dataUtils: DataUtils,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.findAllClients();
+  }
+
+  findAllClients() {
+    this.clients = [];
+    this.clientService.getAllClients().subscribe((value: Clients[]) => {
+      this.clients = value;
+    });
+  }
+
+  trackId(index: number, item: IClients): number {
+    return item.id!;
+  }
+
+  byteSize(base64String: string): string {
+    return this.dataUtils.byteSize(base64String);
+  }
+
+  openFile(base64String: string, contentType: string | null | undefined): void {
+    return this.dataUtils.openFile(base64String, contentType);
+  }
+
+  clientsDetailsRoute(clients: any): void {
+    this.client = clients;
+    this.router.navigateByUrl('/clients-details/' + clients.id);
+  }
 }
